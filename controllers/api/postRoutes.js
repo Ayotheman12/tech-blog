07e251e3450ddef34fec0c92def5adfa1fa0f65d
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-02-26 16:04:52 -0500

    add route to create post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,3 +1,19 @@
 const router = require("express").Router();
+const {Post} = require("../../models");
+
+// Create a new post
+router.post("/", async (req, res) => {
+    try {
+        // Create post
+        const newPost = await Post.create({
+            ...req.body,
+            user_id: req.session.userId
+        });
+
+        res.json({newPost});
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 module.exports = router;
