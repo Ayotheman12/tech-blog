d85ec4a8c996f9748d9e60ac760e7416f337342e
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-02-26 15:39:51 -0500

    add log in route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -13,10 +13,41 @@ router.post("/", async (req, res) => {
             req.session.loggedIn = true;
 
             res.json(userData);
-        })
+        });
     } catch (err) {
         res.status(400).json(err);
     }
-})
+});
+
+// Log in
+router.post("/login", async (req, res) => {
+    try {
+        // Check that the request's username exists
+        const userData = await User.findOne({where: {username: req.body.username}});
+
+        if (!userData) {
+            res.status(400).json({message: "Incorrect username or password"});
+            return;
+        }
+
+        // Check that the request's password matches the one associated with its username
+        const validPassword = userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({message: "Incorrect username or password"});
+            return;
+        }
+
+        // Log in
+        req.session.save(() => {
+            req.session.userId = userData.id;
+            req.session.loggedIn = true;
+
+            res.json(userData);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 module.exports = router;
