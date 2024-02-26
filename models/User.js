d52ab6b49d94e65423d9e25030b65bf385898638
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-02-26 14:21:30 -0500

    add User model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,41 @@
 // id, username, password
+// Import statements
+const {Model, DataTypes} = require("sequelize");
+const bcrypt = require("brcrypt");
+const sequelize = require("../config/connection");
+
+// Create new Model object
+class User extends Model {}
+
+// Initialize the User model
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                length: [8]
+            }
+        }
+    },
+    {
+        hooks: {},
+        sequelize,
+        timestamps: false,
+        underscored: true,
+        modelName: "user"
+    }
+);
+
+module.exports = User;
