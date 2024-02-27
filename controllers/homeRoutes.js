cd74897e3d930d75f1aad3aa392fa375f699256d
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-02-27 15:48:28 -0500

    add route to render homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,28 @@
 const router = require("express").Router();
+const {Post, User} = require("../models");
+
+// Homepage
+router.get("/", async (req, res) => {
+    try {
+        // Get all blog posts
+        const postData = Post.findAll({
+            include: {
+                model: User,
+                attributes: ["username"]
+            }
+        });
+
+        // Serialize the data
+        const posts = (await postData).map((post) => post.get({plain: true}));
+
+        // Render page
+        res.render("homepage", {
+            title: "Homepage",
+            loggedIn: req.session.loggedIn, posts
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
